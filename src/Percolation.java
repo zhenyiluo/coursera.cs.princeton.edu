2bd61ff829960e2630b5f9adb51132c803eef94f
Zhenyi Luo <zhenyiluo@gmail.com>
2015-07-23 20:53:47 -0400

    add skeleton files for Percolation

---
+++ src/Percolation.java
@@ -0,0 +1,36 @@
+
+
+public class Percolation {
+	   // create N-by-N grid, with all sites blocked
+	   public Percolation(int N){
+		   
+	   }
+	   
+	   // open site (row i, column j) if it is not open already
+	   public void open(int i, int j){
+		   
+	   }
+	   
+	   // is site (row i, column j) open?
+	   public boolean isOpen(int i, int j){
+		   // TODO
+		   return false;
+	   }
+	   
+	   // is site (row i, column j) full?
+	   public boolean isFull(int i, int j){
+		   // TODO
+		   return false;
+	   }
+	   
+	   // does the system percolate?
+	   public boolean percolates(){
+		   // TODO
+		   return false;
+	   }
+
+	   // test client (optional)
+	   public static void main(String[] args){
+		   
+	   }
+}
---
+++ src/PercolationStats.java
@@ -0,0 +1,37 @@
+
+
+public class PercolationStats {
+		// perform T independent experiments on an N-by-N grid
+		public PercolationStats(int N, int T){
+			
+		}
+		
+		// sample mean of percolation threshold
+		public double mean(){
+			// TODO
+			return 0.0;
+		}
+		
+		// sample standard deviation of percolation threshold
+		public double stddev(){
+			// TODO
+			return 0;
+		}
+		
+		// low  endpoint of 95% confidence interval
+		public double confidenceLo(){
+			// TODO
+			return 0;
+		}
+		
+		// high endpoint of 95% confidence interval
+		public double confidenceHi(){
+			// TODO
+			return 0;
+		}
+		
+		// test client (described below)
+		public static void main(String[] args){
+			
+		}
+}
